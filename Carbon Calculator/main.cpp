f76f323b737fa3a84f4f4f1201faa269e2b9429a
ravalese <ravalese.j@husky.neu.edu>
2017-09-12 15:28:29 -0400

    Initial Commit

---
+++ Carbon Calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Carbon Calculator
+//
+//  Created by Joseph Ravalese III on 9/12/17.
+//  Copyright © 2017 Joseph Ravalese. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
