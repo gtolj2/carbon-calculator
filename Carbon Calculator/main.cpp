917de5553e6c49753d3a08b35aa49cd3c61b0cdf
ravalese <ravalese.j@husky.neu.edu>
2017-09-12 20:04:07 -0400

    First Commit

--- Carbon Calculator/main.cpp
+++ Carbon Calculator/main.cpp
@@ -7,9 +7,99 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+
+/*
+ 
+************ Functions *************
+
+ Travel:
+ 
+travelByGasCar()
+travelByPlane()
+travelByTrain()
+travelBySubway()
+
+ 
+ 
+ Stay:
+ 
+ 
+
+ 
+*/
+
+
+
+
+
+//Calculates The total carbon footprint of using a gas car using user input of MPG, Distance traveled, etc
+double travelByPlane() {
+    double mpg;
+    double distance;
+    double gallonsNeeded;
+    double totalCarbonfootprint;
+    
+    cout << "Please enter fuel efficiency of Car (miles per gallon)" << endl;
+    cin >> mpg;
+    if (mpg < 0) {
+        cout << "mpg cannot be less than zero" << endl;
+    }
+    
+    else {
+        cout << "please enter total distance travelled in miles" << endl;
+        cin >> distance;
+
+        if (distance < 0) {
+            cout << "distance cannot be less than zero" << endl;
+        }
+        
+        else {
+            gallonsNeeded = distance/mpg;
+            cout << "Gallons of Gasoline needed: " << gallonsNeeded << endl;
+        }
+    }
+    
+    return totalCarbonfootprint;
+}
+
+
+
+//Calculates The total carbon footprint of using a gas car using user input of MPG, Distance traveled, etc
+double travelByGasCar() {
+    double mpg;
+    double distance;
+    double gallonsNeeded;
+    double totalCarbonfootprint;
+    
+    cout << "Please enter fuel efficiency of Car (miles per gallon)" << endl;
+    cin >> mpg;
+    if (mpg < 0) {
+        cout << "mpg cannot be less than zero" << endl;
+    }
+    
+    else {
+        cout << "please enter total distance travelled in miles" << endl;
+        cin >> distance;
+        
+        if (distance < 0) {
+            cout << "distance cannot be less than zero" << endl;
+        }
+        
+        else {
+            gallonsNeeded = distance/mpg;
+            cout << "Gallons of Gasoline needed: " << gallonsNeeded << endl;
+        }
+    }
+    
+    return totalCarbonfootprint;
+}
+
+
+int main() {
+    travelByGasCar();
+    
     return 0;
 }
